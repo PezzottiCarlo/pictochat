0df50cb62b38246675ca3451193575c511119448
Carlo Pezzotti <carlo.pezzotti@student.supsi.ch>
2024-09-26 07:12:14 +0200

    feat: :art: create structure

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const path = require('path');
+
+module.exports = {
+  resolve: {
+    fallback: {
+      "buffer": require.resolve("buffer/"),
+      "stream": require.resolve("stream-browserify"),
+      "util": require.resolve("util/"),
+      "path":false
+    }
+  },
+  // Assicurati di avere anche altre configurazioni di webpack necessarie, ad esempio:
+  entry: './src/index.tsx', // il tuo punto di ingresso
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/
+      },
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader']
+      }
+    ]
+  }
+};

7af319d5f055e9bbb689460e09397ffe59ec5342
Carlo Pezzotti <carlo.pezzotti@student.supsi.ch>
2024-09-30 15:40:26 +0200

    fix: :bug: fix text wrapping, and color

--- src/components/ChatBubble/ChatBubble.tsx
+++ src/components/ChatBubble/ChatBubble.tsx
@@ -1,11 +1,10 @@
 import React, { useEffect, useState } from 'react';
-import { List, Avatar, Flex, Image } from 'antd';
-import { SendOutlined, UserOutlined } from '@ant-design/icons';
+import { List, Flex, Image } from 'antd';
 import { Api } from 'telegram';
-import BubbleMedia from './BubbleMedia';
-import '../../styles/Bubble.css'
-import { HairColor, Pictogram, SkinColor } from '../../lib/AAC';
-import { Words } from '../../lib/Words';
+import BubbleMedia from './ChatBubbleMedia';
+import '../../styles/Bubble.css';
+import { Pictogram } from '../../lib/AAC';
+import { WordsService } from '../../lib/WordsService';
 
 interface ChatBubbleProps {
     message: Api.Message;
@@ -24,22 +23,19 @@ const ChatBubble: React.FC<ChatBubbleProps> = ({ message }) => {
     useEffect(() => {
         if (!message) return;
         if (message.message === null) return;
-        let subject = Words.estraiSoggetto(message.message);
-        if (subject) {
-            setPictoSuggestions(subject);
-        }
+        WordsService.extractSubjects(message.message).then((soggetto) => {
+            setPictoSuggestions(soggetto);
+        });
     }, [message]);
 
     return (
-
-        <List.Item className={`bubble`}>
-            <Flex align="center" justify={message.out ? "flex-end" : "flex-start"} style={{ width: '100%' }} >
+        <List.Item className={`bubble`} onClick={() => WordsService.textToSpeech(message.message)}>
+            <Flex align="center" justify={message.out ? "flex-end" : "flex-start"} style={{ width: '100%' }}>
                 <div className={`bubble-content ${bubbleClass}`}>
                     <div className="message">{message.message}</div>
                     {message.media && <BubbleMedia media={message.media} />}
-                    {(pictoSuggestions)
-                        ?
-                        <div className='bubble-picto'>
+                    {pictoSuggestions && (
+                        <div className="bubble-picto">
                             {pictoSuggestions.map((picto, index) => (
                                 <Image
                                     key={index}
@@ -49,9 +45,7 @@ const ChatBubble: React.FC<ChatBubbleProps> = ({ message }) => {
                                 />
                             ))}
                         </div>
-                        :
-                        <></>
-                    }
+                    )}
                     <div className="timestamp">{formatDate(message.date)}</div>
                 </div>
             </Flex>

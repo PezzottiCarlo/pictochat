8e3f52ea9d119a793bb7b259b7dfe39c34aa8123
Carlo Pezzotti <carlo.pezzotti@student.supsi.ch>
2024-09-30 15:39:52 +0200

    fix: :bug: horizontal scroll

--- src/components/Chat/ChatHintsPicto.tsx
+++ src/components/Chat/ChatHintsPicto.tsx
@@ -8,7 +8,7 @@ interface ChatHintsPictoProps {
     onPictoClick: (picto: Pictogram) => void;
 }
 
-const ChatHintsPicto: React.FC<ChatHintsPictoProps> = ({ pictos,onPictoClick }) => {
+const ChatHintsPicto: React.FC<ChatHintsPictoProps> = ({ pictos, onPictoClick }) => {
 
     const handlePictoClick = (picto: any) => {
         picto.word = picto.keywords[0].keyword;
@@ -16,16 +16,20 @@ const ChatHintsPicto: React.FC<ChatHintsPictoProps> = ({ pictos,onPictoClick }) 
     }
 
     return (
-        <div style={{ display: 'flex', justifyContent: 'center', flexWrap: 'wrap' }}>
+        <div style={{ display: 'flex', overflowX: 'auto', overflowY:"hidden", whiteSpace: 'nowrap', padding: '10px' }}>
             {pictos.map((picto, index) => (
-                <motion.div style={{ display: 'inline-block', marginRight: '10px', textAlign: 'center', width: '100px', height: '100px' }} whileHover={{ scale: 1.1 }} whileTap={{ scale: 0.9 }} onClick={() => handlePictoClick(picto)}>
+                <motion.div
+                    key={index}
+                    style={{ display: 'inline-block', marginRight: '10px', textAlign: 'center', width: '100px', height: '100px' }}
+                    whileHover={{ scale: 1.1 }}
+                    whileTap={{ scale: 0.9 }}
+                    onClick={() => handlePictoClick(picto)}
+                >
                     <Card
-                        key={index}
-                        style={{ display: 'inline-block', marginRight: '10px', textAlign: 'center', width: '100px', height: '100px' }}
+                        style={{ width: '100px', height: '100px' }}
                         hoverable
                         cover={<img alt={`Pictogram ${index}`} src={picto.url} />}
-                    >
-                    </Card>
+                    />
                 </motion.div>
             ))}
         </div>

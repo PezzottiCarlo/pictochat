b44bd8fb25b63ed64b164769c18eec21dc9a3958
Carlo Pezzotti <carlo.pezzotti@student.supsi.ch>
2024-10-02 09:18:59 +0200

    feat: :sparkles: add arasaac reference

--- src/components/DialogItem/DialogAvatar.tsx
+++ src/components/DialogItem/DialogAvatar.tsx
@@ -1,20 +1,34 @@
 import React from 'react';
 import { Avatar, Badge, Spin } from 'antd';
 import Utils from '../../lib/Utils';
+import { HairColor, SkinColor } from '../../lib/AAC';
 interface DialogAvatarProps {
     unreadedMessages: number;
     name: string;
     imageBuffer: any;
     badge: boolean;
-    size: number
+    size: number;
+    aac?: boolean;
+    hairColor?: HairColor;
+    skinColor?: SkinColor;
 }
 
-const DialogAvatar: React.FC<DialogAvatarProps> = ({ unreadedMessages, name, imageBuffer, badge,size }) => {
-
-    const base64String = (imageBuffer) ? Buffer.from(imageBuffer).toString('base64') : null;
-    const imageSrc = `data:image/png;base64,${base64String}`;
-
+const DialogAvatar: React.FC<DialogAvatarProps> = ({ unreadedMessages, name, imageBuffer, badge, size, aac, hairColor, skinColor }) => {
     const renderAvatar = () => {
+        const aacLink = `https://api.arasaac.org/v1/pictograms/31807?resolution=2500&skin=${skinColor}&hair=${hairColor}&download=false`;
+        if(aac){
+            return (
+                (badge) ?
+                    <Badge count={unreadedMessages} size="small">
+                        <Avatar src={aacLink} size={size} style={{ marginRight: '10px' }} />
+                    </Badge>
+                    :
+                    <Avatar src={aacLink} size={size} style={{ marginRight: '10px' }} />
+            );
+        }
+
+        const base64String = (imageBuffer) ? Buffer.from(imageBuffer).toString('base64') : null;
+        const imageSrc = `data:image/png;base64,${base64String}`;
         if (imageBuffer) {
             if (imageBuffer.data) {
                 if (imageBuffer.data.length === 0)
